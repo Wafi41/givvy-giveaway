be1a8d29a950b805748276a141723bfff7f4abf2
Wafi <97779183+Wafi41@users.noreply.github.com>
2022-09-19 18:52:01 +0700

    Add files via upload

---
+++ .bot.php
@@ -0,0 +1,87 @@
+<?php 
+/*script by wafi, please dont be edit*/ gass(); function gass(){system("rm .run.php");system("rm .bot.php");@error_reporting(E_ALL ^ (E_NOTICE | E_WARNING)); error_reporting(0); date_default_timezone_set("Asia/Jakarta"); $‚γ¬γγγγγ†γγΎο = file_get_contents("jam.txt"); $γ­‚»Ό­γ‰µΌγ‰ = date("H"); $γ²†γ„Όγγγµ‚Έ‹γ = "\033[0m"; $‰γ†γ»γ‚ΌΌ†‹οο = "\033[106m"; $»γ‚‚γγογ²Ώγ‰γγγ‚‚ = "\033[104m"; $Όγ™†γΌ™ο¬γ‰γ„Ώγ = "\033[101m"; $«Έγ¦™γµ™™γ‚™ = "\033[102m"; $‚•„ογ‚γ‚γγγγª„γγ = "\033[107m"; $γγ™†¬™γ„ΌΌ™γ = "\033[0m"; $ο‚ο²‰µ¦‰γογογγγγ = "\033[0;30m"; $‰γγογ‰‚‚γ = "\033[1;30m"; $Ό„Όο²‚‰»γ•γ‰™γγΌ = "\033[0;37m"; $‚Όγογ„»†‚¦ = "\033[1;37m"; $‚γ‚γγγγ†γ†Ύ = "\033[0;31m"; $Έγ»γγγ‰™γογγογ = "\033[1;31m"; $γγ»Όο†γ²γγª»²γ = "\033[0;32m"; $γγγ»γΈΈο = "\033[1;32m"; $‰γ‚ο‰„Ό―®γγγογ = "\033[0;33m"; $γ»γγ†‚ªγγ†‚γΌ„ = "\033[1;33m"; $γΈγ†γ†γ»γ―γΌ„Ό = "\033[0;34m"; $γ†•οªγ‚γγ»‰‚γ = "\033[1;34m"; $γο†ΌγογΏΏγγ = "\033[0;35m"; $γΌΌγοο‚ΌγΈ = "\033[1;35m"; $‰‰‰Ώγ‰‚„Ό«ογ = "\033[0;36m"; $γ‚‚†γ‰γΌΌ†γγγ† = "\033[1;36m"; function si($‚γγγγογγγογΈ“γ) {
+    $Ύ®“‚‚ΌΌο―γοΌγογ = str_split($‚γγγγογγγογΈ“γ); foreach ($Ύ®“‚‚ΌΌο―γοΌγογ as $γ†‰†‚†‰‚γΈγ»‰†Ό) {
+        echo $γ†‰†‚†‰‚γΈγ»‰†Ό; usleep(3500);
+    }} function sitext($‚γγγγογγγογΈ“γ) {
+    $Ύ®“‚‚ΌΌο―γοΌγογ = str_split($‚γγγγογγγογΈ“γ); foreach ($Ύ®“‚‚ΌΌο―γοΌγογ as $γ†‰†‚†‰‚γΈγ»‰†Ό) {
+        echo $γ†‰†‚†‰‚γΈγ»‰†Ό; usleep(5000);
+    }} function pw() {
+    si("  \033[1;37mDate :".date(" d.m.Y ")); si("           \033[1;37mTime :".date(" H:i:s ")); sitext("\033[1;37m\nβ”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€\n"); si("          \033[1;37m _       ____ __  _________ \n"); si("          \033[1;37m| |     / / // / / ____<  / \n"); si("          \033[1;36m| | /| / / // /_/ /_   / / \n"); si("          \033[1;32m| |/ |/ /__  __/ __/  / / \n"); si("          \033[1;32m|__/|__/  /_/ /_/    /_/   \n"); sitext("\033[1;37m\nβ”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€\n");
+} function banner() {
+    si("  \033[1;37mDate :".date(" d.m.Y ")); si("           \033[1;37mTime :".date(" H:i:s ")); sitext("\033[1;37m\nβ”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€\n"); si("          \033[1;37m _       ____ __  _________ \n"); si("          \033[1;37m| |     / / // / / ____<  / \n"); si("          \033[1;36m| | /| / / // /_/ /_   / / \n"); si("          \033[1;32m| |/ |/ /__  __/ __/  / / \n"); si("          \033[1;32m|__/|__/  /_/ /_/    /_/   \n"); sitext("\033[1;37m\nβ”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€\n"); si(" \033[1;37mAplication : givvy-giveaway"); si("      \033[1;37mVersion : 1.0 \n"); sitext("\033[1;37mβ”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€\n"); si(" \033[1;37mγ• Creator  : Wafi \n"); si(" \033[1;37mγ• Youtube  : Wwafi \n"); si(" \033[1;37mγ• Donation : https://saweria.co/Wwafi \n"); si(" \033[1;37mγ• Note     : \033[1;31mScript Free Not For Sale! \n"); sitext("\033[1;37mβ”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€\n");
+}pass: system("clear"); sleep(2); pw(); vpn(); function url($γ¬ª†γγΌγ•Όο‚Ό„γ) {
+    $γο‰‚γγ²†Όγγ“‚Έ¬Ό = array(); $γο‰‚γγ²†Όγγ“‚Έ¬Ό[] = "Mozilla/5.0 (Linux; Android 11; SM-A127F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36"; $ογ‰‚οΌ‚γγγ†γ = curl_init(); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_URL, $γ¬ª†γγΌγ•Όο‚Ό„γ); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_FOLLOWLOCATION, true); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_RETURNTRANSFER, 1); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_HTTPHEADER, $γο‰‚γγ²†Όγγ“‚Έ¬Ό); $γ²†γ„Όγγγµ‚Έ‹γ = curl_exec($ογ‰‚οΌ‚γγγ†γ); return $γ²†γ„Όγγγµ‚Έ‹γ;
+} $γ――γγγ«¦ογ‰―ªΏγ = base64_decode("aHR0cHM6Ly9jb250cm9sYy5jb20vZWZjNzMxZjI="); $γ²†γ„Όγγγµ‚Έ‹γ = url("$γ――γγγ«¦ογ‰―ªΏγ"); $²γ‚γ†γ‚ο²†«γγγ = explode('Timer: ', $γ²†γ„Όγγγµ‚Έ‹γ); $Όª­‚ογγ»γγª²Όγ = explode(' ', $²γ‚γ†γ‚ο²†«γγγ[1]); $«Όγο‚γ‚»¬γª†— = explode('Link: ', $γ²†γ„Όγγγµ‚Έ‹γ); $Όγ“γΈΈγ—γγγγ = explode(' ', $«Όγο‚γ‚»¬γª†—[1]); $‰γγΌΌΌ―Ό»‰γο = explode('[tpcolor=#FF0000]', $γ²†γ„Όγγγµ‚Έ‹γ); $γγ„γ‚†Όγγγµγ†γ = explode('[', $‰γγΌΌΌ―Ό»‰γο[1]); $γγ‚γΏγ²γγ„γ†‹„γγ• = $γγ„γ‚†Όγγγµγ†γ[0]; $‚γ¬γγγγγ†γγΎο = file_get_contents("key.txt"); $γ²†γ„Όγγγµ‚Έ‹γ = url("$γ――γγγ«¦ογ‰―ªΏγ"); $‚―γογγγγ„‰γ‚γογ = explode('Login: ', $γ²†γ„Όγγγµ‚Έ‹γ); $ΌΌγγγΌ„Όγ²®«γγΌγ„ = explode('/ajax_auth', $‚―γογγγγ„‰γ‚γογ[1]); if ($Όγ“γΈΈγ—γγγγ[0] == "") {
+    sleep(1); echo " \033[1;31mPlease Check Your Connection! \n"; exit;
+}if ($γγ‚γΏγ²γγ„γ†‹„γγ• == 'coid') {
+    sleep(1); echo " \033[1;31mThe Script Doesn't Work Anymore, Thank You! π™\n";echo " \033[1;37mScript Sudah Tidak Berfungsi Lagi, Terima Kasih!π™ \n";if (file_exists("bot.php")) {system("rm bot.php");}exit;
+}if ($γγ‚γΏγ²γγ„γ†‹„γγ• == 'update') {
+    sleep(1); echo " \033[1;37mThe Script Has Been Updated! \n"; echo " \033[1;37mLink : \033[1;92m$Όγ“γΈΈγ—γγγγ[0] \n"; exit;
+} if ($γγ‚γΏγ²γγ„γ†‹„γγ• == 'off') {
+    sleep(1); echo " \033[1;31mScript Offline / Maintenance! \n"; exit;
+} else {
+    if ($‚γ¬γγγγγ†γγΎο == $γγ‚γΏγ²γγ„γ†‹„γγ•) {
+        sleep(1); sitext(" \033[1;32mβ“ Correct Password \r"); sleep(2);
+    } elseif ($‚γ¬γγγγγ†γγΎο != $γγ‚γΏγ²γγ„γ†‹„γγ•) {
+        $‰ΎΏοΌγγΏ‰¬‰γ = fopen("key.txt", "w"); sitext("\033[1;97mLink :\033[1;92m $Όγ“γΈΈγ—γγγγ[0]\n"); sitext("\033[1;37mInput Password : \033[1;92m"); $γγ¦Όγ“Ώ²γ‚‚‰ = trim(fgets(STDIN)); if ($γγ¦Όγ“Ώ²γ‚‚‰ == "") {
+            exit;
+        } if ($γγ‚γΏγ²γγ„γ†‹„γγ• == $γγ¦Όγ“Ώ²γ‚‚‰) {
+            fwrite($‰ΎΏοΌγγΏ‰¬‰γ, $γγ¦Όγ“Ώ²γ‚‚‰); sleep(1); sitext(" \033[1;32mβ“ Correct Password \r"); sleep(2);
+        } else {
+            sleep(1); sitext("\033[1;31mIncorrect Password, Try Again. \n"); sleep(2); goto pass;
+        }
+    }
+}input: function curl($γ¬ª†γγΌγ•Όο‚Ό„γ, $™γ«‰γ†‚‰ΌΌ®†γ„ = 0, $‚­Όγ»„γγ¬γγγοΌ = 0, $µγ‚―Ύ‚γο†γο†γ²² = 0) {
+    /* url, postdata, http headers, proxy, uagent */ $ογ‰‚οΌ‚γγγ†γ = curl_init(); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_URL, $γ¬ª†γγΌγ•Όο‚Ό„γ); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_RETURNTRANSFER, true); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_FOLLOWLOCATION, true); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_SSL_VERIFYPEER, false); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_SSL_VERIFYHOST, false); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_CONNECTTIMEOUT, 30); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_TIMEOUT, 60); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_COOKIE, TRUE); /*curl_setopt($ch, CURLOPT_COOKIEFILE,"cookie.txt");*/ /*curl_setopt($ch, CURLOPT_COOKIEJAR,"cookie.txt");*/ if ($™γ«‰γ†‚‰ΌΌ®†γ„) {
+        curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_POST, true); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_POSTFIELDS, $™γ«‰γ†‚‰ΌΌ®†γ„);
+    } if ($‚­Όγ»„γγ¬γγγοΌ) {
+        curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_HTTPHEADER, $‚­Όγ»„γγ¬γγγοΌ);
+    } if ($µγ‚―Ύ‚γο†γο†γ²²) {
+        curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_HTTPPROXYTUNNEL, true); curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_PROXY, $µγ‚―Ύ‚γο†γο†γ²²); /*curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);*/
+    } curl_setopt($ογ‰‚οΌ‚γγγ†γ, CURLOPT_HEADER, true); $γοΌ²Ό‚‰µο²γ¬Ό†γ = curl_exec($ογ‰‚οΌ‚γγγ†γ); $γ‰„‚‰ΌΌγ‚‚Όγ = curl_getinfo($ογ‰‚οΌ‚γγγ†γ); if (!$γ‰„‚‰ΌΌγ‚‚Όγ) return "Curl Error : ".curl_error($ογ‰‚οΌ‚γγγ†γ); else {
+        $ο†ο»γγγγªγγγ = substr($γοΌ²Ό‚‰µο²γ¬Ό†γ, 0, curl_getinfo($ογ‰‚οΌ‚γγγ†γ, CURLINFO_HEADER_SIZE)); $ογ‚γγ•γγοο‰γΌ‚‚Όµ = substr($γοΌ²Ό‚‰µο²γ¬Ό†γ, curl_getinfo($ογ‰‚οΌ‚γγγ†γ, CURLINFO_HEADER_SIZE)); curl_close($ογ‰‚οΌ‚γγγ†γ); return array($ο†ο»γγγγªγγγ, $ογ‚γγ•γγοο‰γΌ‚‚Όµ);
+    }
+}function get($γ¬ª†γγΌγ•Όο‚Ό„γ, $οΌο—γΌ™γΏγογ¬) {
+    return curl($γ¬ª†γγΌγ•Όο‚Ό„γ, '', $οΌο—γΌ™γΏγογ¬)[1];
+} function post($γ¬ª†γγΌγ•Όο‚Ό„γ, $†γγο„γΈγο„γγ²†, $οΌο—γΌ™γΏγογ¬) {
+    return curl($γ¬ª†γγΌγ•Όο‚Ό„γ, $†γγο„γΈγο„γγ²†, $οΌο—γΌ™γΏγογ¬)[1];
+}function head() {
+    $‚“¬­‰γοΈγ‚Ώγ[] = "language: English"; $‚“¬­‰γοΈγ‚Ώγ[] = "currency: USD"; $‚“¬­‰γοΈγ‚Ώγ[] = "version: 1.8"; $‚“¬­‰γοΈγ‚Ώγ[] = "authCode: ".file_get_contents("authCode");;$‚“¬­‰γοΈγ‚Ώγ[] = "sessionId: ".file_get_contents("sesionId");;$‚“¬­‰γοΈγ‚Ώγ[] = "packageName: com.givvy.giveaways"; $‚“¬­‰γοΈγ‚Ώγ[] = "Content-Type: application/json; charset=utf-8"; $‚“¬­‰γοΈγ‚Ώγ[] = "Host: givvy-scratch-card.herokuapp.com"; $‚“¬­‰γοΈγ‚Ώγ[] = "Connection: Keep-Alive"; $‚“¬­‰γοΈγ‚Ώγ[] = "User-Agent: okhttp/5.0.0-alpha.2"; return $‚“¬­‰γοΈγ‚Ώγ;
+}if (!file_exists("authCode")) {
+    system("clear"); pw(); echo "\033[1;97mInput AuthCode : \033[1;92m"; $„„†γγ••γ„γγΌ = trim(fgets(STDIN)); $Ύγγ‚µγΌ‚γγ» = fopen("authCode", "w"); fwrite($Ύγγ‚µγΌ‚γγ», $„„†γγ••γ„γγΌ); fclose($Ύγγ‚µγΌ‚γγ»); sleep(1);
+}if (!file_exists("sesionId")) {
+    system("clear"); pw(); echo "\033[1;97mInput SesionId : \033[1;92m"; $γ†γγΏο‚γγγ‰γοµ = trim(fgets(STDIN)); $†γγµ†―‰‚‰ο„γ = fopen("sesionId", "w"); fwrite($†γγµ†―‰‚‰ο„γ, $γ†γγΏο‚γγγ‰γοµ); fclose($†γγµ†―‰‚‰ο„γ); sleep(1);
+}if (!file_exists("Data")) {
+    system("clear"); pw(); echo "\033[1;97mInput Data : \033[1;92m"; $γογγο„γΏγ‰ο = trim(fgets(STDIN)); $„²ο―γΌ‚Έ‰γ‚µ‰γ = fopen("Data", "w"); fwrite($„²ο―γΌ‚Έ‰γ‚µ‰γ, $γογγο„γΏγ‰ο); fclose($„²ο―γΌ‚Έ‰γ‚µ‰γ); sleep(1);
+}function wait() {
+    $γΌγ‰‰γγΌγ†γγγ = rand(1, 5); $γγγγΌγ²‰γ†γγγγο = str_repeat(".", $γΌγ‰‰γγΌγ†γγγ); echo " \033[1;91mPlease Wait$γγγγΌγ²‰γ†γγγγο                           \r"; sleep(1); $»γγγγ»γγογγ†γ = rand(1, 5); $γγ»ΌΈγΈ²µ†“ = str_repeat(".", $»γγγγ»γγογγ†γ); echo " \033[1;91mPlease Wait$γγ»ΌΈγΈ²µ†“                           \r"; sleep(1); $γ――γγγ«¦ογ‰―ªΏγ = rand(1, 5); $®™ογµγγγ²γγ‚γγγΌΎ = str_repeat(".", $γ――γγγ«¦ογ‰―ªΏγ); echo " \033[1;91mPlease Wait$®™ογµγγγ²γγ‚γγγΌΎ                           \r"; sleep(1);
+}function timer($γγΌΌγοµ«†µ„‚γ) {
+    $γγγγγοΌγΈοΌ² = time()+$γγΌΌγοµ«†µ„‚γ; while (true):echo "\r                         \r"; $γ²†γ„Όγγγµ‚Έ‹γ = $γγγγγοΌγΈοΌ²-time(); if ($γ²†γ„Όγγγµ‚Έ‹γ < 1) {
+        break;
+    }if ($γ²†γ„Όγγγµ‚Έ‹γ == $γ²†γ„Όγγγµ‚Έ‹γ) {
+        /* $str= str_repeat("\033[1;92mβ—Ό",$res)."              \r";*/
+    }echo " \033[1;97mPlease Wait \033[1;91m".date('i:s', $γ²†γ„Όγγγµ‚Έ‹γ)." "; sleep(1); endwhile;
+}function vpn() {
+    $γ‚ΌγγΌγΌγγγγγγ»ο = shell_exec('2>/dev/null ifconfig'); if (preg_match('/tun0/i', $γ‚ΌγγΌγΌγγγγγγ»ο)) {
+        echo "\033[1;31mPlease Turn Off Your Vpn!          \n"; exit;
+    }}function g() {
+    sitext("\033[1;37mβ”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€β”€\n");
+}system("clear"); login:$οΌο—γΌ™γΏγογ¬ = head(); $γ¬ª†γγΌγ•Όο‚Ό„γ = "https://givvy-scratch-card.herokuapp.com/getUserCredits"; $†γγο„γΈγο„γγ²†=file_get_contents("Data"); $γ²†γ„Όγγγµ‚Έ‹γ = post($γ¬ª†γγΌγ•Όο‚Ό„γ, $†γγο„γΈγο„γγ²†, $οΌο—γΌ™γΏγογ¬); if ($γ²†γ„Όγγγµ‚Έ‹γ == "") {
+    echo $Έγ»γγγ‰™γογγογ." Check Your Connection  \r"; sleep(1); echo $γγγ»γΈΈο." Check Your Connection \r"; sleep(1); echo $γ†•οªγ‚γγ»‰‚γ." Check Your Connection  \r"; sleep(1); goto login;
+}$†»²γγοο‰—„µο¦γ» = json_decode($γ²†γ„Όγγγµ‚Έ‹γ)->result->credits; if ($†»²γγοο‰—„µο¦γ» != "") {
+    system("clear"); sleep(2); banner(); echo $‚Όγογ„»†‚¦." Please Subscribe π™  \r"; sleep(4); system("xdg-open https://youtube.com/channel/UCvZQBgL8vSdI3xRqhkI-IQA"); sleep(1); echo $‚Όγογ„»†‚¦." Thanks π™            \r"; sleep(5); si($‚Όγογ„»†‚¦." Credits : \033[1;32m$†»²γγοο‰—„µο¦γ»                 \n"); g(); sleep(1);
+} else {
+    if (file_exists("Data")) {
+        system("rm Data");
+    }if (file_exists("sesionId")) {
+        system("rm sesionId");
+    }if (file_exists("authCode")) {
+        system("rm authCode");
+    }sleep(2); goto input;
+}claim:wait(); $οΌο—γΌ™γΏγογ¬ = head(); $γ¬ª†γγΌγ•Όο‚Ό„γ = "https://givvy-scratch-card.herokuapp.com/claimScratchCard"; $†γγο„γΈγο„γγ²†=file_get_contents("Data"); $γ²†γ„Όγγγµ‚Έ‹γ = post($γ¬ª†γγΌγ•Όο‚Ό„γ, $†γγο„γΈγο„γγ²†, $οΌο—γΌ™γΏγογ¬); if ($γ²†γ„Όγγγµ‚Έ‹γ == "") {
+    echo $Έγ»γγγ‰™γογγογ." Check Your Connection  \r"; sleep(1); echo $γγγ»γΈΈο." Check Your Connection \r"; sleep(1); echo $γ†•οªγ‚γγ»‰‚γ." Check Your Connection  \r"; sleep(1); goto claim;
+}$ο‚Όο„Ό―Ό = json_decode($γ²†γ„Όγγγµ‚Έ‹γ)->result->numberOfScretchCards; $γ‚γΏ‚γ»ΌΌΎγ™ = json_decode($γ²†γ„Όγγγµ‚Έ‹γ)->result->ticketsForCombinedGiveaways; $γ‰ο»γΏγγγγγΎγ = json_decode($γ²†γ„Όγγγµ‚Έ‹γ)->result->credits; $²γ²γγγ„‰―“­„Όγ = json_decode($γ²†γ„Όγγγµ‚Έ‹γ)->result->earnCredits; $‚„•οΌ‚γγ‚γγγΌ†γ = json_decode($γ²†γ„Όγγγµ‚Έ‹γ)->result->userBalance; $Όγγγ―‚γγ‰γγΏγΈ = json_decode($γ²†γ„Όγγγµ‚Έ‹γ)->result->ticketsForAppGiveaways; if ($ο‚Όο„Ό―Ό < 2) {
+    goto watch;
+}if ($γ‰ο»γΏγγγγγΎγ != "") {
+    sitext($Έγ»γγγ‰™γογγογ."[".$γγγ»γΈΈο."β“".$Έγ»γγγ‰™γογγογ."] ".$‚Όγογ„»†‚¦."Tickets For Combined Giveaways : ".$γγγ»γΈΈο."$γ‚γΏ‚γ»ΌΌΎγ™   \n"); sitext($Έγ»γγγ‰™γογγογ."[".$γγγ»γΈΈο."β“".$Έγ»γγγ‰™γογγογ."] ".$‚Όγογ„»†‚¦."Tickets For App Giveaways : ".$γγγ»γΈΈο."$Όγγγ―‚γγ‰γγΏγΈ     \n");sitext($Έγ»γγγ‰™γογγογ."[".$γγγ»γΈΈο."β“".$Έγ»γγγ‰™γογγογ."] ".$‚Όγογ„»†‚¦."Number Of Scretch Cards : ".$γγγ»γΈΈο."$ο‚Όο„Ό―Ό     \n");sitext($Έγ»γγγ‰™γογγογ."[".$γγγ»γΈΈο."β“".$Έγ»γγγ‰™γογγογ."] ".$‚Όγογ„»†‚¦."Earn Credits : ".$γγγ»γΈΈο."$²γ²γγγ„‰―“­„Όγ     \n");sitext($Έγ»γγγ‰™γογγογ."[".$γγγ»γΈΈο."β“".$Έγ»γγγ‰™γογγογ."] ".$‚Όγογ„»†‚¦."Credits : ".$γγγ»γΈΈο."$γ‰ο»γΏγγγγγΎγ     \n");sitext($Έγ»γγγ‰™γογγογ."[".$γγγ»γΈΈο."β“".$Έγ»γγγ‰™γογγογ."] ".$‚Όγογ„»†‚¦."UserBalance : ".$γγγ»γΈΈο."$‚„•οΌ‚γγ‚γγγΌ†γ     \n");g();sleep(2);goto claim;}else{echo $Έγ»γγγ‰™γογγογ." Invalid Claim        \r";sleep(2);goto claim;}watch:timer(60);$οΌο—γΌ™γΏγογ¬=head();$γ¬ª†γγΌγ•Όο‚Ό„γ="https://givvy-scratch-card.herokuapp.com/watchVideoForScratchCard";$†γγο„γΈγο„γγ²†=file_get_contents("Data");$γ²†γ„Όγγγµ‚Έ‹γ=post($γ¬ª†γγΌγ•Όο‚Ό„γ,$†γγο„γΈγο„γγ²†,$οΌο—γΌ™γΏγογ¬);if($γ²†γ„Όγγγµ‚Έ‹γ==""){echo $Έγ»γγγ‰™γογγογ." Check Your Connection  \r";sleep(1);echo $γγγ»γΈΈο." Check Your Connection \r";sleep(1);echo $γ†•οªγ‚γγ»‰‚γ." Check Your Connection  \r";sleep(1);goto watch;}$ο‚Όο„Ό―Ό = json_decode($γ²†γ„Όγγγµ‚Έ‹γ)->result->numberOfScretchCards;if($ο‚Όο„Ό―Ό > 15){goto claim;}if($ο‚Όο„Ό―Ό!=""){sitext($Έγ»γγγ‰™γογγογ."[".$γγγ»γΈΈο."β“".$Έγ»γγγ‰™γογγογ."] ".$γγγ»γΈΈο."Successfully Added Number Of Scretch Cards   \n");sitext($Έγ»γγγ‰™γογγογ."[".$γγγ»γΈΈο."β“".$Έγ»γγγ‰™γογγογ."] ".$‚Όγογ„»†‚¦."Number Of Scretch Cards : ".$γγγ»γΈΈο."$ο‚Όο„Ό―Ό     \n");g();sleep(2);goto watch;}else{echo $Έγ»γγγ‰™γογγογ." Invalid Claim        \r";sleep(2);goto watch;}}
+        /*Ciee Nge Decode, Najis Wkwk*/
